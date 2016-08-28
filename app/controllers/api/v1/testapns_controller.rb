555a1cc086e2b6041f970c39e48d59e6b09d11ef
cristian.vezure <cristian.vezure@woowteam.com>
2016-08-28 13:49:06 +0300

    apns

--- app/controllers/api/v1/testapns_controller.rb
+++ app/controllers/api/v1/testapns_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::TestapnsController < ApplicationController
     APNS.host = 'gateway.sandbox.push.apple.com'
        # gateway.sandbox.push.apple.com is default
 
-       APNS.pem  = '/Users/Jarvis/Documents/_fac/workspace/LuciApi/config/apns-dev-cert.pem'
+       APNS.pem  = '#{Rails.root}/config/apns-dev-cert.pem'
        # this is the file you just created
 
        APNS.port = 2195
